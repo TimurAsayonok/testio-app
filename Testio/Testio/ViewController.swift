ab883b732517cb7f3ebd98b71c630d3e4873de18
TimurAsayonok <t.asaynok@gmail.com>
2022-09-15 18:56:59 +0200

    created and added base code for the app

---
+++ Testio/Testio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Testio
+//
+//  Created by Timur Asayonok on 15/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
