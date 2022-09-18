fa9dffb57ea6812491e4c5469d4bb8c8d79cf7a3
TimurAsayonok <t.asaynok@gmail.com>
2022-09-18 23:37:07 +0200

    deleted scene delegate and added main file for entry point

---
+++ Testio/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Testio
+//
+//  Created by Timur Asayonok on 18/09/2022.
+//
+
+import UIKit
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(AppDelegate.self)
+)
